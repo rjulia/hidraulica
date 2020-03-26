f81b8e400d933b42db045028c395904ad2bc3845
Ramon Julia <ramon@tofugear.com>
2020-03-26 08:32:07 +0800

    add related projects

--- src/pages/Service/Service.js
+++ src/pages/Service/Service.js
@@ -4,7 +4,7 @@ import Arrow from "assets/icons/ic_arrow_down_black.svg";
 
 import React, { useEffect, useState } from 'react'
 import './Services.scss'
-import { SliderHome, Title, WrapperSection, HeroImage } from 'components';
+import { SliderHome, Title, WrapperSection, HeroImage, Card } from 'components';
 import { getService } from '../../services/contentful'
 import {
   useParams
@@ -19,12 +19,13 @@ const Service = (props) => {
   const [isLoading, setLoading] = useState(true)
   const [service, setService] = useState([])
   const promiseService = getService(i18n.language, slug)
-
+  const [relateds, setRelateds] = useState([])
   useEffect(() => {
     setLoading(true)
     promiseService
       .then(data => {
         setService(data[0])
+        setRelateds(_.get(data[0], 'fields.proyectosRelacionados'))
       }).finally(() => {
         setLoading(false)
       })
@@ -73,6 +74,17 @@ const Service = (props) => {
         <div className="first_block--title col-12 col-md-12">
           <Title text={t('services.third_block.title')} />
           <Link className="first_block--link" to="/projectos">{t('services.third_block.button')} <img src={Arrow} alt="" /> </Link>
+        </div>
+        <div className='col-12 ml-5 mt-5'>
+          <div className="row">
+            {
+              relateds && _.map(relateds, related => (
+                <div className="col-12 col-md-4" key={related.sys.id} >
+                  <Card project={related} />
+                </div>
+              ))
+            }
+          </div>
         </div>
 
       </WrapperSection>
